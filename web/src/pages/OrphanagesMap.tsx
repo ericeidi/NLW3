3bb6a10c2fcd58f35678066b08ebe0c12f88513b
Eric <ericen01@gmail.com>
2020-10-14 15:51:30 -0300

    front end finalized

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
-import { FiPlus } from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
-import 'leaflet/dist/leaflet.css';
+import mapMarkerImg from '../images/map-marker.svg'
 
-import mapMarkImg from '../images/map-marker.svg'
+import MapMarker from '../utils/mapIcon'
 
 import '../styles/pages/orphanages-map.css'
+import api from '../services/api';
 
+interface Orphanage {
+    id: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+}
 
 function OrphanagesMap() {
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+    useEffect(() => { 
+        api.get('orphanages').then(response => {
+            setOrphanages(response.data)
+        })
+    }, []);
+
+
     return (
         <div id="page-map">
             <aside>
                 <header>
-                    <img src={mapMarkImg} alt="Happy"/>
+                    <img src={mapMarkerImg} alt="Happy"/>
                     <h2>Choose an orphanage on the map</h2>
                     <p>There are many children waiting for your visit :)</p>
                 </header>
@@ -33,9 +49,23 @@ function OrphanagesMap() {
             >
                 {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"> */}
                 <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
-                </Map>
+                
 
-            <Link to="" className="create-orphanage">
+                {orphanages.map(orphanage => {
+                    return(
+                        <Marker icon={MapMarker} position={[orphanage.latitude,orphanage.longitude]} key ={orphanage.id}> 
+                        <Popup closeButton={false} minWidth= {240} maxWidth= {240} className="map-popup">
+                            {orphanage.name}
+                            <a href={`/orphanages/${orphanage.id}`}>
+                                <FiArrowRight size={20} color="#FFF"/>
+                            </a>
+                        </Popup>
+                    </Marker>
+                    )
+                })}
+            </Map>
+
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF"/>
             </Link>
         </div>
---
+++ web/src/utils/mapIcon.ts
@@ -0,0 +1,14 @@
+import Leaftlet from 'leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+
+const mapIcon = Leaftlet.icon({
+    iconUrl: mapMarkerImg,
+  
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+  })
+  
+  export default mapIcon
